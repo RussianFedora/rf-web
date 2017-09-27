8057d4ba72ad6b94a39b7d8c37582dc7f3aed7b9
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-09-27 13:23:21 +0200

    Sync caches with russianfedora.pro before and after deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -8,4 +8,6 @@ echo "Publishing to ${DEST}"
 rsync -rav --delete --rsh="${RSH_STRING}" output/ russianfedora.pro:~/"${DEST}"
 
 # Run nikola's post deploy hooks
+rsync -rav --delete --rsh="${RSH_STRING}" russianfedora.pro:~/"${DEST}"/cache cache/ || true
 nikola deploy
+rsync -rav --delete --rsh="${RSH_STRING}" cache/ russianfedora.pro:~/"${DEST}"/cache
