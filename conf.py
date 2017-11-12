ab401b650a2dc1e27d13e4b499b8153a0f8224b3
Aleksandra Fedorova (bookwar) <alpha@bookwar.info>
2017-11-12 15:16:29 +0100

    Set default site URL to HTTPS

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Russian Fedora"  # (translatable)
 BLOG_TITLE = "Russian Fedora"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://ru.fedoracommunity.org/"
+SITE_URL = "https://ru.fedoracommunity.org/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://ru.fedoracommunity.org/"
