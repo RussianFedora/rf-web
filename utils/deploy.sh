fa5774527067e04e621ffac8f9722c1369096322
Aleksandra Fedorova <alpha@bookwar.info>
2019-04-30 14:07:49 +0200

    Change host and disable comments
    Host hardcoded to the IP address. Current host is provided by @bookwar.
    G+ comments are no longer available.

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,12 +2,13 @@
 
 RSH_STRING="/usr/bin/sshpass -p ${DEPLOY_PASSWORD} ssh -o StrictHostKeyChecking=no -l ${DEPLOY_USER}"
 
+HOST="212.227.198.243"
 DEST=${TRAVIS_BRANCH:-test}
 echo "Publishing to ${DEST}"
 
-rsync -rav --delete --rsh="${RSH_STRING}" output/ russianfedora.pro:~/"${DEST}"
+rsync -rav --delete --rsh="${RSH_STRING}" output/ ${HOST}:~/"${DEST}"
 
 # Run nikola's post deploy hooks
-rsync -rav --delete --rsh="${RSH_STRING}" russianfedora.pro:~/"${DEST}"/cache cache/ || true
+rsync -rav --delete --rsh="${RSH_STRING}" ${HOST}:~/"${DEST}"/cache cache/ || true
 nikola deploy
-rsync -rav --delete --rsh="${RSH_STRING}" cache/ russianfedora.pro:~/"${DEST}"/cache
+rsync -rav --delete --rsh="${RSH_STRING}" cache/ ${HOST}:~/"${DEST}"/cache
